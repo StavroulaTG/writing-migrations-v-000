2f9ca9d2532a2792fc50f9179386e04f55013a02
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-03 12:48:35 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migrate[5.1]
   def change
     change_column :students, :table_name, :string
-    change_column :students, :column_name, :string
+    change_column :students, :column_name
     change_column :students, :type, :datetime
   end
 end
